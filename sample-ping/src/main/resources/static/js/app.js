273d22625964647ecff520e7a967841f6a0f780a
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2015-05-29 21:17:57 -0700

    With sample angular based UI

---
+++ sample-ping/src/main/resources/static/js/app.js
@@ -0,0 +1,40 @@
+var pingApp = angular.module("pingApp", ["ui.router"]);
+
+pingApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("home");
+
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: URLS.partialsHome,
+            controller: 'HomeController'
+        });
+});
+
+
+pingApp.factory("pingFactory", function ($http) {
+    var factory = {};
+
+    factory.getMessageResponse = function(message) {
+        return $http.post(URLS.dispatch, {payload: message});
+    }
+
+    return factory;
+});
+
+
+pingApp.controller("HomeController", function ($scope, pingFactory) {
+    function init() {
+        $scope.message = "";
+        $scope.errorMessage = "";
+    }
+
+    $scope.getMessageResponse = function(message) {
+        pingFactory.getMessageResponse(message).success(function(data){
+            $scope.message = data.payload;
+        }).error(function(data, status, headers, config) {
+            $scope.errorMessage = data.error;
+        });
+    }
+    init();
+});
