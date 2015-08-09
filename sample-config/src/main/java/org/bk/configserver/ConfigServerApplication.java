2c0dd8622f44bc7c19c5f0986eb794171a0ca476
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2015-08-09 10:10:57 -0700

    With Archaius Support

--- sample-config/src/main/java/org/bk/configserver/ConfigServerApplication.java
+++ sample-config/src/main/java/org/bk/configserver/ConfigServerApplication.java
@@ -13,4 +13,5 @@ public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
+
 }
--- sample-ping/src/main/java/org/bk/consumer/PingApplication.java
+++ sample-ping/src/main/java/org/bk/consumer/PingApplication.java
@@ -1,11 +1,15 @@
 package org.bk.consumer;
 
+import com.netflix.config.ConcurrentMapConfiguration;
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,4 +22,11 @@ public class PingApplication {
         SpringApplication.run(PingApplication.class, args);
     }
 
+
+    @Bean
+    public AbstractConfiguration sampleArchaiusConfiguration() throws Exception {
+        ConcurrentMapConfiguration concurrentMapConfiguration = new ConcurrentMapConfiguration();
+        concurrentMapConfiguration.addProperty("testkey", "testvalue");
+        return concurrentMapConfiguration;
+    }
 }
